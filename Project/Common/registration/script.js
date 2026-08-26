0332d0fd840a2f53bdfd254bc9fb65b41f194730
Emad <emadharun.eh@gmail.com>
2026-08-26 22:24:18 +0600

    upate page based on selected role

---
+++ Project/Common/registration/script.js
@@ -0,0 +1,43 @@
+let customerDivs = Array.from(document.getElementsByClassName("customerDiv"));
+let restaurantDivs = Array.from(document.getElementsByClassName("restaurantDiv"));
+let riderDivs = Array.from(document.getElementsByClassName("riderDiv"));
+
+function toggleRoleFields() {
+    const customer = document.getElementById("customerRoleSelector").checked;
+    const restaurant = document.getElementById("restaurantRoleSelector").checked;
+    const rider = document.getElementById("riderRoleSelector").checked;
+
+    if (customer) {
+        showCustomerFields();
+    } else if (restaurant) {
+        showRestaurantFields();
+    } else if (rider) {
+        showRiderFields();
+    }
+}
+
+function showCustomerFields() {
+    restaurantDivs.forEach(hideDiv);
+    riderDivs.forEach(hideDiv);
+    customerDivs.forEach(showDiv);
+}
+
+function showRestaurantFields() {
+    customerDivs.forEach(hideDiv);
+    riderDivs.forEach(hideDiv);
+    restaurantDivs.forEach(showDiv);
+}
+
+function showRiderFields() {
+    customerDivs.forEach(hideDiv);
+    restaurantDivs.forEach(hideDiv);
+    riderDivs.forEach(showDiv);
+}
+
+function showDiv(div) {
+    div.style.display = "block";
+}
+
+function hideDiv(div) {
+    div.style.display = "none";
+}
