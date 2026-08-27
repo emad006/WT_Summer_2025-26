a1e285dbc66919bd683ad709f85bb04b79f2be82
Emad <emadharun.eh@gmail.com>
2026-08-27 14:26:23 +0600

    change password implemented

--- Project/Common/changePassword/changePassword.php
+++ Project/Common/changePassword/changePassword.php
@@ -2,8 +2,70 @@
 session_start();
 
 include "../lib/dbConfig.php";
+include "../lib/helperFunctions.php";
 
 $errors = [];
+$success = "";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    if (isset($_POST["submitBtn"])) { // Enter when submit button is pressed
+        
+        // <-------- Validation Start -------->
+
+        // Current password validation
+        if (empty($_POST["currPassword"])) {
+            $errors[] = "Current password is required.";
+        } else if (strlen($_POST["currPassword"]) < 8) { // Check if the password is at least 8 characters long
+            $errors[] = "Current password must be at least 8 characters long.";
+        } else if (strlen($_POST["currPassword"]) > 255) { // Check if the password is less than 255 characters long
+            $errors[] = "Current password cannot exceed more than 255 characters long.";
+        }
+
+        // New password validation
+        if (empty($_POST["newPassword"])) { // Check if password is empty
+            $errors[] = "New password is required.";
+        } else if (strlen($_POST["newPassword"]) < 8) { // Check if the password is at least 8 characters long
+            $errors[] = "New password must be at least 8 characters long.";
+        } else if (strlen($_POST["newPassword"]) > 255) { // Check if the password is less than 255 characters long
+            $errors[] = "New password cannot exceed more than 255 characters long.";
+        } else if ($_POST["newPassword"] === $_POST["currPassword"]) { // Check if the old and new passwords match
+            $errors[] = "New password cannot be the same as the current password.";
+        } else if ($_POST["newPassword"] !== $_POST["confNewPassword"]) { // Check if the passwords match
+            $errors[] = "New passwords do not match.";
+        } else { // All above validations passed
+
+            // Get correct password from database
+            $stmt = mysqli_prepare($conn, "SELECT password FROM users WHERE user_id = ?");
+            mysqli_stmt_bind_param($stmt, "s", $_SESSION["user_id"]);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            $dbPassword = mysqli_fetch_assoc($result)["password"];
+
+            // Check if provided password and actual password match
+            if ($_POST["currPassword"] !== $dbPassword) {
+                $errors[] = "Your password is incorrect.";
+            }
+        }
+        // <-------- Validation End -------->
+
+        // <-------- Query the Database -------->
+
+        if (count($errors) === 0) {
+            // Proceed to updating password
+            $stmt = mysqli_prepare($conn, "UPDATE users SET password = ?, remember_token = NULL WHERE user_id = ?");
+            mysqli_stmt_bind_param($stmt, "si", $_POST["newPassword"], $_SESSION["user_id"]); // Update password and reset cookie
+            mysqli_stmt_execute($stmt);
+
+            setcookie("remember_me", "", time()- 3600, "/");
+            $success = "Password changed successfully."; // TODO: Implement displaying success message
+
+            // Redirect user to dashboard
+            redirectUser($_SESSION["role"]);
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +79,7 @@ $errors = [];
 <body>
     <div id="navbar">
         <div id="navLeft">
+            <!-- TODO: Add logic for correct nav bar labels + link based on role -->
             <a href="#" class="navLink">Dashboard</a>
             <a href="#" class="navLink">Browse</a>
             <a href="#" class="navLink">Cart</a>
@@ -25,6 +88,7 @@ $errors = [];
             <a href="#" class="navLink">Logout</a>
         </div>
 
+        <!-- TODO: Dynamic name + role display -->
         <div id="navRight">Emad · Customer</div>
     </div>
 
@@ -53,8 +117,8 @@ $errors = [];
             </div>
 
             <div class="inputBlock">
-                <button type="submit" class="btn" id="submitBtn">Change Password</button>
-                <button type="submit" class="btn" id="cancelBtn">Cancel</button>
+                <button type="submit" name="submitBtn" class="btn" id="submitBtn">Change Password</button>
+                <button type="submit" name=submitBtn class="btn" id="cancelBtn">Cancel</button>
             </div>
         </form>
     </div>
