67250bfb7283e195743c609d9e2f7b2c93605819
Emad <emadharun.eh@gmail.com>
2026-08-28 12:57:47 +0600

    added session checking

--- Project/Customer/dashboard/dashboard.php
+++ Project/Customer/dashboard/dashboard.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+
+if (!isset($_SESSION["user_id"]) || $_SESSION["role"] !== "customer") {
+    header("Location:../../Common/login/login.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,17 +17,16 @@
     <body>
         <div id="navbar">
         <div id="navLeft">
-            <!-- TODO: Add logic for correct nav bar labels + link based on role -->
             <a href="#" class="navLink">Dashboard</a>
             <a href="#" class="navLink">Browse</a>
             <a href="#" class="navLink">Cart</a>
             <a href="#" class="navLink">My Orders</a>
             <a href="#" class="navLink navLinkActive">Profile</a>
-            <a href="#" class="navLink">Logout</a>
+            <a href="../../Common/logout.php" class="navLink">Logout</a>
         </div>
 
         <!-- TODO: Dynamic name + role display -->
-        <div id="navRight">Emad · Customer</div>
+        <div id="navRight"><?php echo $_SESSION['name'] . " · Customer"; ?></div>
         </div>
     </body>
 </html>
