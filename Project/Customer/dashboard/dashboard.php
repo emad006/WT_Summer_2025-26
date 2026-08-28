5514503faacb37b133bc282eeb8c0215fd763af1
Emad <emadharun.eh@gmail.com>
2026-08-28 12:45:25 +0600

    nav bar added

---
+++ Project/Customer/dashboard/dashboard.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>Customer - Dashboard</title>
+    </head>
+
+    <body>
+        <div id="navbar">
+        <div id="navLeft">
+            <!-- TODO: Add logic for correct nav bar labels + link based on role -->
+            <a href="#" class="navLink">Dashboard</a>
+            <a href="#" class="navLink">Browse</a>
+            <a href="#" class="navLink">Cart</a>
+            <a href="#" class="navLink">My Orders</a>
+            <a href="#" class="navLink navLinkActive">Profile</a>
+            <a href="#" class="navLink">Logout</a>
+        </div>
+
+        <!-- TODO: Dynamic name + role display -->
+        <div id="navRight">Emad · Customer</div>
+        </div>
+    </body>
+</html>
