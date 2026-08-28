89ea53ea6488a08c28d02679a95c5ee25c384982
Emad <emadharun.eh@gmail.com>
2026-08-28 13:25:22 +0600

    dashboard front-end complete

--- Project/Customer/dashboard/dashboard.php
+++ Project/Customer/dashboard/dashboard.php
@@ -5,6 +5,8 @@ if (!isset($_SESSION["user_id"]) || $_SESSION["role"] !== "customer") {
     header("Location:../../Common/login/login.php");
     exit();
 }
+
+$_SESSION["name"] = "Emad";
 ?>
 
 <!DOCTYPE html>
@@ -25,8 +27,34 @@ if (!isset($_SESSION["user_id"]) || $_SESSION["role"] !== "customer") {
             <a href="../../Common/logout.php" class="navLink">Logout</a>
         </div>
 
-        <!-- TODO: Dynamic name + role display -->
         <div id="navRight"><?php echo $_SESSION['name'] . " · Customer"; ?></div>
+        </div>
+
+
+
+        <div id="mainArea">
+        <h1 id="titleName">Welcome <?php echo $_SESSION["name"]; ?></h1>
+
+        <div class="tableBlock">
+            <table border="1">
+                <tr>
+                    <th>Active Orders</th>
+                    <th>Orders this Month</th>
+                    <th>Total Spent</th>
+                </tr>
+            </table>
+        </div>
+
+        <div class="tableBlock">
+            <label id="activeOrdersLabel">Your Active Orders</label>
+            <table border="1">
+                <tr>
+                    <th>Order</th>
+                    <th>Restaurant</th>
+                    <th>Total</th>
+                    <th>Status</th>
+                </tr>
+            </table>
         </div>
     </body>
 </html>
